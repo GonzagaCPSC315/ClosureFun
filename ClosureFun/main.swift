34ed81b59baaeda0f98ff2558f9b5e8876f87128
gsprint <sprint@gonzaga.edu>
2020-09-22 12:53:05 -0700

    Initial Commit

---
+++ ClosureFun/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ClosureFun
+//
+//  Created by Gina Sprint on 9/22/20.
+//  Copyright © 2020 Gina Sprint. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
